313bbe4acd2cf369c631b26c1c3412e656ecf03b
mattiasphbg <mattiasp@hotmail.com>
2025-02-19 08:59:48 +0100

    Update home screen layout with background image and refined grid structure
    - Added full-screen background image
    - Updated grid layout with responsive column spans
    - Replaced placeholder text with welcome heading
    - Adjusted grid item configurations

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,18 @@
 import { StatusBar, StyleSheet, View } from "react-native";
 import { Grid, GridItem } from "@/components/ui/grid";
-
+import { Heading } from "@/components/ui/heading";
 import { Text } from "@/components/ui/text";
-
+import { Image } from "@/components/ui/image";
 export default function HomeScreen() {
   return (
     <View className="pt-10">
+      <Image
+        source={{
+          uri: "https://res.cloudinary.com/dxhfq1g84/image/upload/v1739951850/pexels-mohamed-hamdi-510308652-16282306_mupgva.jpg",
+        }}
+        alt="background"
+        className="absolute inset-0 w-full h-full -z-10"
+      />
       <StatusBar barStyle="dark-content" />
       <Grid
         className="gap-5 w-screen h-screen"
@@ -19,13 +26,13 @@ export default function HomeScreen() {
             className: "col-span-12",
           }}
         >
-          <Text>One</Text>
+          <Heading bold>Welcome to the app</Heading>
         </GridItem>
 
         <GridItem
           className="bg-background-50 p-6 rounded-md border-solid border-2 border-red-500"
           _extra={{
-            className: "col-span-8",
+            className: "col-span-4",
           }}
         >
           <Text className="text-2xl font-bold ">Two</Text>
@@ -49,7 +56,7 @@ export default function HomeScreen() {
         <GridItem
           className="bg-background-50 p-6 rounded-md border-solid border-2 border-red-500"
           _extra={{
-            className: "col-span-2",
+            className: "col-span-",
           }}
         >
           <Text className="text-2xl font-bold">Five</Text>
