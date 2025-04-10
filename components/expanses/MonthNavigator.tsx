abc8d8e216266c94160ec5b166edc2d67fd3e8e3
mattiasphbg <mattiasp@hotmail.com>
2025-04-10 12:00:20 +0200

    Enhance MonthNavigator component with date picker functionality
    - Add a modal with a calendar for selecting dates in the MonthNavigator component
    - Implement state management for the visibility of the date picker
    - Update date handling to reflect user selections in the current date display
    - Include new dependencies for calendar functionality in package.json

--- components/expanses/MonthNavigator.tsx
+++ components/expanses/MonthNavigator.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { View, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { View, TouchableOpacity, Modal } from "react-native";
 import { Text } from "../ui/text";
+import { Calendar } from "react-native-calendars";
 
 interface MonthNavigatorProps {
   currentDate: Date;
@@ -11,10 +12,17 @@ const MonthNavigator: React.FC<MonthNavigatorProps> = ({
   currentDate,
   setCurrentDate,
 }) => {
+  const [isPickerVisible, setPickerVisible] = useState(false);
+
   const changeMonth = (delta: number) => {
     const newDate = new Date(currentDate);
     newDate.setMonth(newDate.getMonth() + delta);
     setCurrentDate(newDate);
+  };
+
+  const handleDateChange = (date: Date) => {
+    setCurrentDate(date);
+    setPickerVisible(false);
   };
 
   return (
@@ -22,7 +30,7 @@ const MonthNavigator: React.FC<MonthNavigatorProps> = ({
       <TouchableOpacity onPress={() => changeMonth(-1)} activeOpacity={0.7}>
         <Text className="text-indigo-600 text-base">← Prev</Text>
       </TouchableOpacity>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => setPickerVisible(true)}>
         <Text className="text-lg font-semibold text-indigo-600">
           {currentDate.toLocaleString("default", {
             month: "long",
@@ -33,6 +41,27 @@ const MonthNavigator: React.FC<MonthNavigatorProps> = ({
       <TouchableOpacity onPress={() => changeMonth(1)} activeOpacity={0.7}>
         <Text className="text-indigo-600 text-base">Next →</Text>
       </TouchableOpacity>
+
+      <Modal visible={isPickerVisible} transparent={true}>
+        <View className="flex-1 justify-center items-center">
+          <Calendar
+            current={currentDate.toISOString().split("T")[0]}
+            onDayPress={(day: { dateString: string }) =>
+              handleDateChange(new Date(day.dateString))
+            }
+            markedDates={{
+              [currentDate.toISOString().split("T")[0]]: {
+                selected: true,
+                marked: true,
+                selectedColor: "blue",
+              },
+            }}
+          />
+          <TouchableOpacity onPress={() => setPickerVisible(false)}>
+            <Text className="text-indigo-600 text-base">Cancel</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 };
