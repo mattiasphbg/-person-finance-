3c3b9c3a156a66c6f4cec0d80e305711b8450f69
mattiasphbg <mattiasp@hotmail.com>
2025-07-18 15:14:55 +0200

    Refactor ExpanseWidget to simplify structure and update functionality
    - Removed multiple clickable FlexWidgets and replaced them with a single TextWidget displaying "Hello".
    - Updated clickAction to open a URI linking to Google.
    - Streamlined the component for improved readability and maintenance.

--- app/widgets/ExpanseWidget.tsx
+++ app/widgets/ExpanseWidget.tsx
@@ -6,60 +6,10 @@ import { FlexWidget, TextWidget } from "react-native-android-widget";
 export default function expanseWidget() {
   return (
     <FlexWidget
-      style={{
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#FFFFFF",
-        height: "match_parent",
-        width: "match_parent",
-        borderRadius: 32,
-        flex: 1,
-        flexDirection: "column",
-      }}
+      clickAction="OPEN_URI"
+      clickActionData={{ uri: "https://www.google.com" }}
     >
-      <FlexWidget
-        style={{
-          height: "wrap_content",
-          width: "wrap_content",
-          alignItems: "center",
-          justifyContent: "center",
-          padding: 16,
-        }}
-        clickAction="OPEN_APP"
-      >
-        <TextWidget style={{ fontSize: 12 }} text="Open App" />
-      </FlexWidget>
-      <FlexWidget
-        style={{
-          height: "wrap_content",
-          width: "wrap_content",
-          alignItems: "center",
-          justifyContent: "center",
-          padding: 16,
-        }}
-        clickAction="OPEN_URI"
-        clickActionData={{ uri: "myapp://expenses" }}
-      >
-        <TextWidget
-          style={{ fontSize: 12 }}
-          text="Open Fitness Preview DeepLink"
-        />
-      </FlexWidget>
-      <FlexWidget
-        style={{
-          height: "wrap_content",
-          width: "wrap_content",
-          alignItems: "center",
-          justifyContent: "center",
-          padding: 16,
-        }}
-        clickAction="OPEN_URI"
-        clickActionData={{
-          uri: "https://github.com/sAleksovski/react-native-android-widget",
-        }}
-      >
-        <TextWidget style={{ fontSize: 12 }} text="Open GitHub Link" />
-      </FlexWidget>
+      <TextWidget text="Hello" />
     </FlexWidget>
   );
 }
