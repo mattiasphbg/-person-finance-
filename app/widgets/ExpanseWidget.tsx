9bcba5e0376b2035cc4d8597991ab16c6a50f418
mattiasphbg <mattiasp@hotmail.com>
2025-07-23 16:18:10 +0200

    Update ExpanseWidget with TODO comments for future enhancements

--- app/widgets/ExpanseWidget.tsx
+++ app/widgets/ExpanseWidget.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FlexWidget, TextWidget } from "react-native-android-widget";
-
+//
 // TODO: we need to  make when click + button change to the "expense page"
 // TODO: a list with all elements added this month
 export default function expanseWidget() {
