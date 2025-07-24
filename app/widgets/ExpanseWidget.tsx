ef9b4af040ab553a6eb59c1cb28313aea4f88e93
mattiasphbg <mattiasp@hotmail.com>
2025-07-24 13:32:02 +0200

    Refactor ExpanseWidget to enhance user interaction and styling
    - Updated ExpanseWidget to change the click action to open the main app with a specific screen for expenses.
    - Improved layout and styling for better user experience, including updated text and background properties.
    - Removed TODO comments to streamline the code.

--- app/widgets/ExpanseWidget.tsx
+++ app/widgets/ExpanseWidget.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { FlexWidget, TextWidget } from "react-native-android-widget";
-//
-// TODO: we need to  make when click + button change to the "expense page"
-// TODO: a list with all elements added this month
-export default function expanseWidget() {
+
+export default function ExpanseWidget() {
   return (
-    <FlexWidget clickAction="directToExpensePage">
-      <TextWidget text="Hello" />
+    <FlexWidget
+      style={{
+        height: "match_parent",
+        width: "match_parent",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#efefef",
+        borderRadius: 16,
+      }}
+      // Action: Open the main app
+      clickAction="OPEN_APP"
+      // Data: Tell the app to go to the 'expenses' screen
+      clickActionData={{ screen: "expenses" }}
+    >
+      <TextWidget
+        text="Add New Expense"
+        style={{ fontSize: 22, color: "#000000" }}
+      />
     </FlexWidget>
   );
 }
