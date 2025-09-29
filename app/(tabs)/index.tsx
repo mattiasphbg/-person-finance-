2f0d07068276daa75d21bd42a24690871de00a2b
mattiasphbg <mattiasp@hotmail.com>
2025-09-29 16:49:37 +0200

    Remove Android project files and configurations
    - Deleted the entire Android project directory, including build files, Gradle configurations, and resource files.
    - This cleanup is part of a transition to a different project structure or framework.
    - Ensured that all related files, including widget and application resources, were removed for a clean slate.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,11 @@
 import Dashboard from "@/components/dashboard";
 import { SafeAreaView, StatusBar } from "react-native";
 
-import { WidgetPreview } from "react-native-android-widget";
-import ExpenseWidget from "@/app/widgets/ExpanseWidget";
-
 export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1 bg-gray-50">
       <StatusBar barStyle="dark-content" />
       <Dashboard />
-      <WidgetPreview renderWidget={ExpenseWidget} height={100} width={100} />
     </SafeAreaView>
   );
 }
